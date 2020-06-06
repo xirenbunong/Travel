f6cd5944b2de57f609f0de03192d46355a3c37d7
lichaofei <lichaofei@haodf.com>
2020-06-06 18:40:10 +0800

    feat: add header and search

---
+++ src/pages/city/City.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <city-header></city-header>
+        <city-search></city-search>
+    </div>
+</template>
+
+<script>
+import CityHeader from './components/Header'
+import CitySearch from './components/Search'
+export default {
+    name: 'City',
+    components: {
+        CityHeader,
+        CitySearch
+    },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
